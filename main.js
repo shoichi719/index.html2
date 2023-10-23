9da2aed0a508a993fb19c7adf5523f6658e67e72
shoichi nagasaki <clark.zoro@gmail.com>
2023-10-23 20:19:00 +0900

    learning of javascript

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log('a');
+console.log('b');
+console.log('c');
+
+function sample(a){
+  let result;
+  if(a>0) {
+    result = '正の数'
+  } else {
+    result = '負の数'
+  } return result;
+}
+console.log(sample(-10));
